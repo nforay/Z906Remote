cdf6688ff0eb03175fa121500d3cb09e6b0b0ef2
Nicolas Foray <contact@nforay.dev>
2024-02-16 19:16:04 +0100

    chore: fix linter settings

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', 'prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': ['error', { endOfLine: 'crlf' }],
     'vue/multi-word-component-names': 'off',
     'vue/require-default-prop': 'off',
     'vue/html-indent': ['error', 2],
